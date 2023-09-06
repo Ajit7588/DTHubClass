16550d5f60ad9feb027b4c226391e3e9b13ceb4b
AjitKumarTung <ajitkumartung@gmail.com>
2023-09-06 23:12:51 +0530

    first code push

---
+++ DtHub/src/test/java/javaproject/Sample1.java
@@ -0,0 +1,20 @@
+package javaproject;
+
+public class Sample1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		
+		Sample1 s1=new Sample1();
+		s1.main1(args);
+
+	}
+	
+	public static void main1(String[] args)
+	{
+		System.out.println("hello 2");
+	}
+	
+
+}
